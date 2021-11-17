8d0256d579800cf7d733e9655084bc8d84c83500
Georges <georgeswebmaster@gmail.com>
2021-11-17 14:04:59 +0100

    Version avec affichage Produits

--- front/script.js
+++ front/script.js
@@ -1,8 +1,24 @@
 let xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
-    console.log(xmlhttp.responseText);
+    displayProducts(JSON.parse(xmlhttp.responseText));
   }
 };
 xmlhttp.open("GET", "http://localhost:3000/api/products", true);
 xmlhttp.send();
+
+function displayProducts(data){
+for(let i=0;i<data.length;i++){
+  let items=document.getElementById("items");
+  items.innerHTML+='<a href="./product.html?id=42">'+
+  '<article>'+
+  '<img src=".../product01.jpg"alt="Lorem ipsum dolor sit amet, Kanap name1"/>'+
+  '<h3 class="productName">'+data[i].name+'</h3>'+
+          '<p class="productDescription">'+
+                'Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis'+
+                'enim malesuada risus sapien gravida nulla nisl arcu.'+
+              '</p>'+
+            '</article>'+
+          '</a>';
+}
+}
